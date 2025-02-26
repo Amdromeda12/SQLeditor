01a3e85d8c6d9f4d48218b6d41b7a8e6434d555c
Tobernt <tobias_berntsson@hotmail.com>
2025-02-26 09:35:22 +0100

    packages

--- obj/Debug/net8.0-windows/SQLDatabaseViewer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SQLDatabaseViewer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLDatabaseViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5758de8a5edae5daffad36c73f7da61158ccb562")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLDatabaseViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLDatabaseViewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
